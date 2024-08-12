fcacc100c1e52c22c4975eabda9938fbd52c6ac5
whitekite <1075790909@qq.com>
2024-08-12 18:51:43 +0800

    feat: 检测 `web-vue` 仓库是否有多余的图标，如果有的话合并

--- build/loadIcons.ts
+++ build/loadIcons.ts
@@ -1,7 +1,88 @@
-import { downloadTemplate } from 'giget'
-import { getNPMVersion, importDirectory, runSVGO } from '@iconify/tools'
+import { type IconSet, getNPMVersion, importDirectory, runSVGO } from '@iconify/tools'
 import consola from 'consola'
+import { downloadTemplate } from 'giget'
 import type { PluginConfig } from 'svgo'
+
+/**
+ * 加载处理图标
+ * @param name 图标名称
+ * @param from 来源的 iconset
+ * @param to 将图标处理进的 iconset
+ */
+function copyIcon(name: string, from: IconSet, to?: IconSet) {
+  const svg = from.toSVG(name)
+
+  const plugins: PluginConfig[] = [
+    'removeUnknownsAndDefaults',
+    'cleanupAttrs',
+    'removeXMLNS',
+    'removeDoctype',
+    'removeXMLProcInst',
+    'removeComments',
+    'removeMetadata',
+    'removeTitle',
+    'removeDesc',
+    'removeUselessDefs',
+    'removeEditorsNSData',
+    'removeEmptyAttrs',
+    'removeHiddenElems',
+    'removeEmptyText',
+    'removeEmptyContainers',
+    'cleanupEnableBackground',
+    'convertStyleToAttrs',
+    'convertColors',
+    'convertPathData',
+    'convertTransform',
+    'removeNonInheritableGroupAttrs',
+    'removeUselessStrokeAndFill',
+    'removeUnusedNS',
+    'cleanupIds',
+    'cleanupNumericValues',
+    'moveElemsAttrsToGroup',
+    'moveGroupAttrsToElems',
+    'collapseGroups',
+    'mergePaths',
+    'convertShapeToPath',
+    'sortAttrs',
+    'removeDimensions',
+  ]
+  if (name.endsWith('color')) {
+    plugins.push('removeStyleElement')
+    plugins.push('removeEmptyContainers')
+  }
+  runSVGO(svg!, {
+    plugins,
+  })
+  ;(to || from).fromSVG(name, svg!)
+}
+
+/**
+ * 从 web-vue 仓库,检查是否有额外的图标
+ * @param iconSet
+ */
+async function loadVueIcons(iconSet: IconSet) {
+  consola.info('check icons from web-vue github repo')
+  consola.info('download icons svg file from web-vue github repo')
+  await downloadTemplate('github:arco-design/arco-design-vue/packages/web-vue/icon/_svgs', {
+    forceClean: true,
+    dir: '.svg',
+  })
+  consola.success('download web-vue icons svg file success')
+  consola.info('load web-vue icons svg file')
+  const vueIconSet = await importDirectory('.svg', {
+    prefix: 'arco-design',
+  })
+  vueIconSet.forEach((name, type) => {
+    if (type !== 'icon') {
+      return
+    }
+    if (iconSet.exists(name)) {
+      return
+    }
+    consola.info('load additional icons from web-vue:', name)
+    copyIcon(name, vueIconSet, iconSet)
+  })
+}
 
 export async function loadIcons() {
   consola.info('download icons svg file from github repo')
@@ -19,53 +100,10 @@ export async function loadIcons() {
     if (type !== 'icon') {
       return
     }
-
-    const svg = iconSet.toSVG(name)
-
-    const plugins: PluginConfig[] = [
-      'removeUnknownsAndDefaults',
-      'cleanupAttrs',
-      'removeXMLNS',
-      'removeDoctype',
-      'removeXMLProcInst',
-      'removeComments',
-      'removeMetadata',
-      'removeTitle',
-      'removeDesc',
-      'removeUselessDefs',
-      'removeEditorsNSData',
-      'removeEmptyAttrs',
-      'removeHiddenElems',
-      'removeEmptyText',
-      'removeEmptyContainers',
-      'cleanupEnableBackground',
-      'convertStyleToAttrs',
-      'convertColors',
-      'convertPathData',
-      'convertTransform',
-      'removeNonInheritableGroupAttrs',
-      'removeUselessStrokeAndFill',
-      'removeUnusedNS',
-      'cleanupIds',
-      'cleanupNumericValues',
-      'moveElemsAttrsToGroup',
-      'moveGroupAttrsToElems',
-      'collapseGroups',
-      'mergePaths',
-      'convertShapeToPath',
-      'sortAttrs',
-      'removeDimensions',
-    ]
-    if (name.endsWith('color')) {
-      plugins.push('removeStyleElement')
-      plugins.push('removeEmptyContainers')
-    }
-    runSVGO(svg!, {
-      plugins,
-    })
-
-    iconSet.fromSVG(name, svg!)
+    copyIcon(name, iconSet)
   })
+
+  await loadVueIcons(iconSet)
 
   const version = await getNPMVersion({ package: '@arco-design/web-react' })
 
